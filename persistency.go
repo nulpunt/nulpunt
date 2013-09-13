d1bb90c9afb5125a6b0b38534117d162e90a51d2
GeertJohan <gjr19912@gmail.com>
2013-09-13 21:24:46 +0200

    Added comments on persistency (users collection)

--- persistency.go
+++ persistency.go
@@ -10,14 +10,19 @@ var (
 )
 
 func setupPersistency() {
+	// dial to localhost mongoDB instance
 	mgoConn, err := mgo.Dial("localhost")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// get "nulpunt" database
 	dbNulpunt := mgoConn.DB("nulpunt")
 
+	// get "users" collection
 	colUsers = dbNulpunt.C("users")
+
+	// ensure that key "username" is unique for collection "users".
 	err := colUsers.EnsureIndex(mgo.Index{
 		Key:    []string{"username"},
 		Unique: true,
