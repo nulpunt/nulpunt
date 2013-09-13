e2f326576c948084445bb22e013fea0ca0c72d59
GeertJohan <gjr19912@gmail.com>
2013-09-13 21:29:11 +0200

    comments on setup* functions

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import (
 func main() {
 	log.Println("starting nulpunt server application")
 
+	// setup connection to mongodb, check indexes, etc.
 	setupPersistency()
 	log.Println("persistency set up")
 
+	// start a http server
 	setupHTTPServer()
 	log.Println("http server set up")
 
+	// all seems good
 	log.Println("running..")
 	// wait forever
 	select {}
