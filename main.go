0109049cde990f1cda09d4b3346498ca78211793
GeertJohan <gjr19912@gmail.com>
2013-09-13 21:20:28 +0200

    Simple server setup

--- main.go
+++ main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	log.Println("TODO")
+	log.Println("starting nulpunt server application")
+
+	setupPersistency()
+	log.Println("persistency set up")
+
+	setupHTTPServer()
+	log.Println("http server set up")
+
+	log.Println("running..")
+	// wait forever
+	select {}
 }
---
+++ persistency.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"labix.org/v2/mgo"
+)
+
+var (
+	colUsers *mgo.Collection
+)
+
+func setupPersistency() {
+
+}
