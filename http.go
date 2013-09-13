0c92e4b9802222efd758d65ca5362e20a43659fa
GeertJohan <gjr19912@gmail.com>
2013-09-13 21:27:48 +0200

    setupHTTPServer() working

--- http.go
+++ http.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func setupHTTPServer() {
-	//++ TODO
+	// serve static files from http-files on root
+	http.Handle("/", http.FileServer(http.Dir("./http-files/")))
+
+	// run http server in goroutine
+	go func() {
+		port := "8000"
+		log.Println("starting http server on port %s\n", port)
+		err := http.ListenAndServe(":"+port, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
