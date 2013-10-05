da70e8b3a5bb5413a9b135fefc3748cb4a95216f
GeertJohan <gjr19912@gmail.com>
2013-10-05 12:35:50 +0200

    Moved session handlers to seperate file.

--- gopath/src/npserver/http.go
+++ gopath/src/npserver/http.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
-	"io"
 	"log"
 	"net"
 	"net/http"
@@ -107,40 +105,4 @@ func initHTTPServer() {
 			}
 		}()
 	}
-}
-
-// sessionInitHandlerFunc creates a new session
-func sessionInitHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	type outDataType struct {
-		SessionKey string `json:"sessionKey"`
-	}
-
-	// create a new cs
-	newCS := newClientSession()
-
-	// return cs key
-	out := &outDataType{
-		SessionKey: newCS.key,
-	}
-	err := json.NewEncoder(w).Encode(out)
-	if err != nil {
-		log.Printf("Could not encode output data for service sessionInitHandlerFunc. %s\n", err)
-		http.Error(w, "internal error", http.StatusInternalServerError)
-	}
-}
-
-// sessionCheckHandlerFunc checks if session is ok
-func sessionCheckHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "session ok")
-}
-
-// sessionDestroyHandlerFunc destroys a session
-func sessionDestroyHandlerFunc(w http.ResponseWriter, r *http.Request) {
-	cs, err := getClientSession(r.Header.Get(headerKeySessionKey))
-	if err != nil {
-		http.Error(w, "error", http.StatusInternalServerError)
-		return
-	}
-	cs.destroy()
-	cs.done()
 }
