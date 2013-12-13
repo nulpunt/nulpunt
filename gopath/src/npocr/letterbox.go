9303a5fd7ec1a5bc15a7df6e3a6f4c7cdaf63932
GeertJohan <gjr19912@gmail.com>
2013-12-13 16:55:24 +0100

    Renamed go.letterbox to npocr npocr will use nsq for queue purposes.

--- gopath/src/npocr/letterbox.go
+++ gopath/src/npocr/letterbox.go
@@ -1,10 +1,4 @@
-/* Pacakge letterbox
- * Manages a queue of documents that will be OCR'ed.
- *
- * Copyright, 2013, by: Guido Witmond (guido@witmond.nl)
-*/
-
-package main // letterbox
+package main
 
 import (
 	"github.com/GeertJohan/go.leptonica"
@@ -18,7 +12,7 @@ import (
 // API
 //
 // letterbox.Enqueue(document letterbox.Document)
-// Accepts a document and places it in the parsing queue. 
+// Accepts a document and places it in the parsing queue.
 // All needed data is stored inside a mongo-record.
 
 // letterbox.RunQueue()
@@ -26,32 +20,31 @@ import (
 // It is assumed that the queue runner lives in a separate process than the np-server.  P
 // robably on a different machine.
 
-
 // Document is a base type in the queue. Both incoming as outgoing queues
 type Document struct {
-	ID       bson.ObjectId `bson:"_id"`
+	ID           bson.ObjectId `bson:"_id"`
 	DocumentName string
-	Language string
-	Image []byte
+	Language     string
+	Image        []byte
 	// Add more document field to your tasting.
-	Pages []Page    // will be filled after succesful parsing.
-	ParseError error // will be set at errors
+	Pages      []Page // will be filled after succesful parsing.
+	ParseError error  // will be set at errors
 }
 
-// Pages are part of documents. 
+// Pages are part of documents.
 type Page struct {
 	PageNumber string // yes, string. it can be anything from the document: "ix", "12", "appendix A-3"
-	FullText string // the unformatted full text for searching
-	BoxText tesseract.BoxText
+	FullText   string // the unformatted full text for searching
+	BoxText    tesseract.BoxText
 }
 
 type Queue struct {
-	In *mgo.Collection
+	In  *mgo.Collection
 	Out *mgo.Collection
 }
 
 // Create a new queue on the mongo-db on hostname
-func NewQueue(host string) *Queue { 
+func NewQueue(host string) *Queue {
 	mgoConn, err := mgo.Dial(host)
 	check(err)
 	db := mgoConn.DB("nulpunt")
@@ -73,21 +66,21 @@ func main() {
 	// prepare data
 	imageName := "pkiTaskforce.png"
 	image, err := ioutil.ReadFile(imageName)
-	check(err)  // report this error to the user!
+	check(err) // report this error to the user!
 
 	newDoc := Document{
-		ID:       bson.NewObjectId(), 
-		Image: image,
+		ID:           bson.NewObjectId(),
+		Image:        image,
 		DocumentName: "Overheid PKI aanbesteding", // the 'official' name of the document
-		Language: "nld",
+		Language:     "nld",
 		// Set more field to your taste.
-	}		
+	}
 
 	check(queue.Enqueue(newDoc))
 	// prentend end of the upload-functionality.
-	 //return
+	//return
 
-//-------------------------------------------------------------------------------------------------------------------
+	//-------------------------------------------------------------------------------------------------------------------
 
 	// Meanwhile at a server far far away:
 
@@ -96,8 +89,7 @@ func main() {
 	return
 }
 
-
-func(que *Queue) RunQueue() {
+func (que *Queue) RunQueue() {
 	query := que.In.Find(nil)
 	query.Batch(1) // documents can be big, parsing takes long, don't waste memory.
 	iter := query.Iter()
@@ -107,12 +99,14 @@ func(que *Queue) RunQueue() {
 		ProcessDocument(que, doc) // ProcessDoc saves the results in the DB.
 	}
 	err := iter.Close()
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Processes a single document.
 func ProcessDocument(que *Queue, doc *Document) {
-	if doc.ParseError != nil { 
+	if doc.ParseError != nil {
 		return // skip previous errors that are stuck in the queue.
 	}
 	pages, parserr := ParseDocument(doc)
@@ -121,24 +115,32 @@ func ProcessDocument(que *Queue, doc *Document) {
 		// Only valid parsed documents go to the OUT-queue.
 		doc.ParseError = parserr
 		err := que.In.Update(bson.M{"ID": doc.ID}, doc) // "ID" spelled correctly?
-		if err != nil { panic(err) }  // double error, panic
+		if err != nil {
+			panic(err)
+		} // double error, panic
 	}
 
 	// parsing went ok.
 	doc.Pages = pages
-	
+
 	// store it in mongo.
 	// TODO: Atomically
 	err := que.Out.Insert(doc)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	err = que.In.RemoveId(doc.ID)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func ParseDocument(doc *Document) ([]Page, error) {
 	// create new tess instance and point it to the tessdata location.
 	tess, err := tesseract.NewTess("/usr/local/share/tessdata", doc.Language)
-	if err != nil {return nil, err }
+	if err != nil {
+		return nil, err
+	}
 	defer tess.Close()
 
 	pix, err := leptonica.NewPixReadMem(&doc.Image)
@@ -148,7 +150,7 @@ func ParseDocument(doc *Document) ([]Page, error) {
 		return nil, err
 	}
 	tess.SetImagePix(pix)
-	
+
 	// retrieve boxed text from the tesseract instance
 	bt, err := tess.BoxText(0)
 	if err != nil {
@@ -158,11 +160,10 @@ func ParseDocument(doc *Document) ([]Page, error) {
 
 	return []Page{Page{
 		PageNumber: "1",
-		FullText: tess.Text(),
-		BoxText: *bt,
+		FullText:   tess.Text(),
+		BoxText:    *bt,
 	}}, nil
 }
-
 
 // Utils
 
@@ -174,4 +175,3 @@ func check(err error) {
 		panic(err)
 	}
 }
-
